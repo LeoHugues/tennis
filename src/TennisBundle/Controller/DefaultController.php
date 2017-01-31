fd6987cf08784cc9f7bd1fdb24242fa708c5f124
LeoHugues <leo.hugues@hotmail.fr>
2017-01-31 11:12:53 +0100

    mise en place des interfaces graphiques

--- src/TennisBundle/Controller/DefaultController.php
+++ src/TennisBundle/Controller/DefaultController.php
@@ -8,10 +8,26 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="tennis_home")
      */
     public function indexAction()
     {
         return $this->render('TennisBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/gestion-joueur", name="gestion_joueur")
+     */
+    public function gestionJoueurAction()
+    {
+        return $this->render('TennisBundle:Default:gestion-joueur.html.twig');
+    }
+
+    /**
+     * @Route("/gestion-rencontre", name="gestion_rencontre")
+     */
+    public function gestionRencontreAction()
+    {
+        return $this->render('TennisBundle:Default:gestion-rencontre.html.twig');
     }
 }
