fd5b7386514d3d3aaf166b4a27c70490263029b1
DISKOPierre <pierre.b@disko.fr>
2017-03-14 08:46:49 +0100

    WIP

--- src/OrganisationBundle/DataFixtures/ORM/LoadJoueurData.php
+++ src/OrganisationBundle/DataFixtures/ORM/LoadJoueurData.php
@@ -6,7 +6,20 @@
  * Date: 03/03/2017
  * Time: 14:35
  */
-class LoadJoueurData
-{
 
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use UserBundle\Entity\User;
+
+class LoadJoueurData implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $userAdmin = new User();
+        $userAdmin->setUsername('admin');
+        $userAdmin->setPassword('test');
+
+        $manager->persist($userAdmin);
+        $manager->flush();
+    }
 }
